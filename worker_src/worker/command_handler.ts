b4def0b38c570d4baba0daeded264b2e772f37e9
Artemis21 <artemisdev21@gmail.com>
2021-03-25 17:07:20 +0000

    Add inadvertently ignored folder.

---
+++ worker_src/worker/command_handler.ts
@@ -0,0 +1,98 @@
+/** Handle slash command callbacks. */
+import { commands } from '../commands/index';
+import { Command } from '../command_types';
+import {
+    CommandResponse,
+    Member,
+    SlashCommandData,
+    SlashCommandInteractionData,
+    SlashCommandInteractionDataOption,
+    User,
+} from '../api_types';
+
+/** Parse and store command interaction data. */
+export class CommandContext {
+    id: string;
+    responseToken: string;
+    guildId: string | null;
+    channelId: string | null;
+    member: Member | null;
+    user: User;
+    commandNameParts: Array<string>;
+    options: Record<string, any>;
+    command: Command | null;
+
+    /** Store the data. */
+    constructor(data: SlashCommandData) {
+        this.id = data.id;
+        this.responseToken = data.token;
+        this.channelId = data.channel_id || null;
+        if ('guild_id' in data) {
+            this.guildId = data.guild_id;
+            this.member = data.member;
+            this.user = data.member.user;
+        } else {
+            this.guildId = null;
+            this.member = null;
+            this.user = data.user;
+        }
+        this.commandNameParts = [];
+        this.options = {};
+        this.parseOptions(data.data);
+        this.command = this.getCommand();
+    }
+
+    /** Parse a key/value pair or subcommand. */
+    parseOptions(
+        data: SlashCommandInteractionData | SlashCommandInteractionDataOption
+    ) {
+        if ('value' in data) {
+            // It's a key/value pair.
+            this.options[data.name] = data.value;
+        } else {
+            // It's a subcommand or subcommand group.
+            this.commandNameParts.push(data.name);
+            if (data.options) {
+                for (const option of data.options) {
+                    this.parseOptions(option);
+                }
+            }
+        }
+    }
+
+    /** Get the callback for this specific command. */
+    getCommand() {
+        let command = commands[this.commandNameParts[0]];
+        for (const namePart in this.commandNameParts.slice(1)) {
+            if ('subcommands' in command) {
+                command = command.subcommands[namePart];
+            }
+        }
+        if ('subcommands' in command) {
+            console.warn(
+                `Command "${this.commandNameParts.concat(' ')}" not found. ` +
+                    "Make sure you've registered the latest commands."
+            );
+            return null;
+        }
+        return command;
+    }
+
+    /** Call this command's callback, providing this context. */
+    async call() {
+        if (!this.command) {
+            return { content: 'Internal error - command not found.' };
+        }
+        return await this.command.callback(this, this.options);
+    }
+}
+
+/** Get and use data from a command callback. */
+export async function commandHandler(
+    _request: Request,
+    data: SlashCommandData
+) {
+    const context = new CommandContext(data);
+    const commandResponse: CommandResponse = await context.call();
+    return { type: 4, data: commandResponse };
+}
---
+++ worker_src/worker/index.ts
@@ -0,0 +1,61 @@
+/** Handle signature validation and determine event type. */
+import nacl from 'tweetnacl';
+import { commandHandler } from './command_handler';
+import {
+    InteractionData,
+    InteractionResponse,
+    InteractionType,
+    PingInteractionData,
+} from '../api_types';
+
+/** Convert a hex string to a Uint8Array */
+function hexToBytes(hexString: string): Uint8Array | null {
+    const pairs = hexString.match(/.{2}/g);
+    if (!pairs) return null;
+    return new Uint8Array(pairs.map(byte => parseInt(byte, 16)));
+}
+
+/** Validate the signature header. */
+function validateSignature(request: Request, body: string): boolean {
+    const rawSignature = request.headers.get('X-Signature-Ed25519');
+    const timestamp = request.headers.get('X-Signature-Timestamp');
+    if (rawSignature == null || timestamp == null) return false;
+    const message = new TextEncoder().encode(timestamp + body);
+    const signature = hexToBytes(rawSignature);
+    if (!signature) return false;
+    const publicKey = hexToBytes(DISCORD_PUBLIC_KEY)!;
+    return nacl.sign.detached.verify(message, signature, publicKey);
+}
+
+/** Respond to a ping event with a pong. */
+async function pingHandler(
+    _request: Request,
+    _data: PingInteractionData
+): Promise<InteractionResponse> {
+    return { type: 1 };
+}
+
+/** Handle any request. */
+async function handleRequest(request: Request) {
+    const body = await request.text();
+    if (!validateSignature(request, body)) {
+        return new Response('Invalid signature.', { status: 401 });
+    }
+    const data: InteractionData = JSON.parse(body);
+    let resp: InteractionResponse;
+    switch (data.type) {
+        case InteractionType.PING:
+            resp = await pingHandler(request, data);
+            break;
+        case InteractionType.SLASH_COMMAND:
+            resp = await commandHandler(request, data);
+            break;
+        default:
+            return new Response('Unkown event type.', { status: 422 });
+    }
+    return new Response(JSON.stringify(resp), {
+        headers: { 'Content-Type': 'application/json' },
+    });
+}
+
+export { handleRequest };
